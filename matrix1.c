7b9eb2c8ab2b2cfc7f9e53e467fb64ea5a40becd
p10moys <stelios.detroit@gmail.com>
2018-03-13 12:56:56 +0200

    αρχεια

--- matrix1.c
+++ matrix1.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
-
 
 #define NCOLS 100
+#define NROWS 100 //δοκιμή 100,1000,10000,10000 γραμμές
 
 void get_walltime(double *wct) {
   struct timeval tp;
@@ -16,8 +15,8 @@ void get_walltime(double *wct) {
 
 int main() {
 double *table;
-double ts,te;
-
+double ts,te,t;
+double skr;
 
   table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
   if (table==NULL) {
@@ -25,30 +24,27 @@ double ts,te;
     exit(1);
   }
 
-  // warmup
-
-  // ...your code here...
-
-  // get starting time (double, seconds) 
+  for (int i = 0; i < NROWS * NCOLS; i++){
+	table[i] = 1.0;
+  }
+  
   get_walltime(&ts);
   
-  // workload
+  for ( int i = 0; i < NROWS; i++){
+	  
+	for ( int j = 0; j < NCOLS; j++){
 
-  // ...your code here...
+		skr = table[i]; //για την μη απαλειφη των λουπ
 
-  // get ending time
+	}
+  }
+	
   get_walltime(&te);
 
-  // check results
-  
-  // ...your code here...
-
-  // print time elapsed and/or Maccesses/sec
-  
-  // ...your code here...  
+  t = te - ts; // η διαφορά των δυο timestamps είναι ο χρόνος που χρειάστηκε
+  printf("%f\n",t);
   
   free(table);
 
   return 0;
 }
-
--- matrix2.c
+++ matrix2.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix2.c -o matrix2
-
 
 #define NCOLS 100
+#define NROWS 100 //δοκιμή 100,1000,10000,10000 γραμμές
 
 void get_walltime(double *wct) {
   struct timeval tp;
@@ -16,8 +15,8 @@ void get_walltime(double *wct) {
 
 int main() {
 double *table;
-double ts,te;
-
+double ts,te,t;
+double skr;
 
   table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
   if (table==NULL) {
@@ -25,27 +24,26 @@ double ts,te;
     exit(1);
   }
 
-  // warmup
-
-  // ...your code here...
-
-  // get starting time (double, seconds) 
+  for (int i = 0; i < NROWS * NCOLS; i++){
+	table[i] = 1.0;
+  }
+  
   get_walltime(&ts);
   
-  // workload
+  
+	for ( int j = 0; j < NCOLS; j++){
+		
+		for ( int i = 0; i < NROWS; i++){
 
-  // ...your code here...
+		skr = table[i]; //για την μη απαλειφη των λουπ
 
-  // get ending time
+	}
+  }
+	
   get_walltime(&te);
 
-  // check results
-  
-  // ...your code here...
-
-  // print time elapsed and/or Maccesses/sec
-  
-  // ...your code here...  
+  t = te - ts; // η διαφορά των δυο timestamps είναι ο χρόνος που χρειάστηκε
+  printf("%f\n",t);
   
   free(table);
 
